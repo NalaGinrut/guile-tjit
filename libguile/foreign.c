a1dce117689e7ed3a1f6257523b7aa91efabe824
8c6794b6 <8c6794b6@gmail.com>
2015-03-10 10:34:05 +0900

    Added extra word in program to hold jit code
    * libguile/programs.c:
    * libguile/programs.h:
      Allocate extra word in program to hold pointer to jit compile
      code. Added new `XXX_F_IS_XXX' C macro for jit compiled code.
      Added function to set and get the compiled code with scheme
      interface. Modified offset of program free variables from 2 to 3.
    
    * module/system/vm/program.scm:
      Export jit code setter and getter defined in C.
    
    * module/system/vm/assembler.scm:
      Modified the word size used for static-procedure from 2 to 3. To avoid
      JIT compiled code for static procedure to overwrite existing contents
      in byte-compiled code.
    
    * libguile/continuations.c:
    * libguile/control.c:
    * libguile/foreign.c:
    * libguile/gsubr.c:
    * libguile/vm-engine.c:
      Modified `nfree + 2' to `nfree + 3', for the new extra word in program
      SCM object.
    
    * module/system/vm/lightning.scm:
      Modified to save jit compiled code in added extra field of SCM program
      object. Run-time call to procedures without JIT code can first check
      this field and then jump to the pointer if it exist. Minor tidy up and
      some removals of unused codes.
    
    * module/system/vm/lightning/trace.scm:
      Added simple entry node scanner, currently unused.
    
    * test-suite/vm/lightning/t.scm:
      Commented out tests causing segfault.

--- libguile/foreign.c
+++ libguile/foreign.c
@@ -835,11 +835,14 @@ cif_to_procedure (SCM cif, SCM func_ptr, int with_errno)
 
   c_cif = (ffi_cif *) SCM_POINTER_VALUE (cif);
 
-  ret = scm_words (scm_tc7_program | (nfree << 16) | flags, nfree + 2);
+  ret = scm_words (scm_tc7_program | (nfree << 16) | flags, nfree + 3);
   SCM_SET_CELL_WORD_1 (ret, get_foreign_stub_code (c_cif->nargs, with_errno));
+
   SCM_PROGRAM_FREE_VARIABLE_SET (ret, 0, cif);
   SCM_PROGRAM_FREE_VARIABLE_SET (ret, 1, func_ptr);
-  
+  // An extra word in program to hold jit code
+  SCM_PROGRAM_FREE_VARIABLE_SET (ret, 2, SCM_UNDEFINED);
+
   return ret;
 }
 
